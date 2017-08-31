5460e5e627f259636678a8808cece563ec35a55d
Elianta <allie.que.sonrie@gmail.com>
2017-08-31 15:42:32 +0300

    Added: opportunity to move the userDialog popup window over the screen.

--- js/dialog.js
+++ js/dialog.js
@@ -5,6 +5,11 @@
   var userDialogClose = userDialog.querySelector('.setup-close');
   var userDialogSave = userDialog.querySelector('.setup-submit');
   var userName = userDialog.querySelector('.setup-user-name');
+  var dialogHandle = userDialog.querySelector('.upload');
+  var userDialogPosition = {
+    x: window.getComputedStyle(userDialog).left,
+    y: window.getComputedStyle(userDialog).top
+  };
 
   var onPopupEscPress = function (event) {
     window.util.isEscEvent(event, function () {
@@ -15,6 +20,8 @@
   };
   var openPopupUserDialog = function () {
     window.util.showElement(userDialog);
+    userDialog.style.top = userDialogPosition.y;
+    userDialog.style.left = userDialogPosition.x;
     document.addEventListener('keydown', onPopupEscPress);
   };
   var closePopupUserDialog = function () {
@@ -36,5 +43,44 @@
   userDialogSave.addEventListener('keydown', function (evt) {
     window.util.isEnterEvent(evt, closePopupUserDialog);
   });
-})();
 
+  dialogHandle.addEventListener('mousedown', function (event) {
+    event.preventDefault();
+    var startPosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+    var newPosition = {
+      x: event.clientX,
+      y: event.clientY
+    };
+    var onMouseMove = function (moveEvent) {
+      moveEvent.preventDefault();
+      var shift = {
+        x: newPosition.x - moveEvent.clientX,
+        y: newPosition.y - moveEvent.clientY
+      };
+      newPosition = {
+        x: moveEvent.clientX,
+        y: moveEvent.clientY
+      };
+      userDialog.style.top = (userDialog.offsetTop - shift.y) + 'px';
+      userDialog.style.left = (userDialog.offsetLeft - shift.x) + 'px';
+    };
+    var onMouseUp = function (upEvent) {
+      upEvent.preventDefault();
+    };
+    var onClick = function (clickEvt) {
+      if (startPosition.x !== newPosition.x || startPosition.y !== newPosition.y) {
+        clickEvt.preventDefault();
+      }
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+      document.removeEventListener('click', onClick);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+    document.addEventListener('click', onClick);
+  });
+})();
