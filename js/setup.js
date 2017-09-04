7e0c104a1884e4b4eb4754865c3ce07378dd5859
Elianta <allie.que.sonrie@gmail.com>
2017-09-04 21:33:13 +0300

    Similar wizards data is taken from server (backend.load method) instead of being generated randomly. Form data is sent with Dataform object using backend.save method.

--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,9 @@
 'use strict';
 (function () {
-  var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия',
-    'Люпита', 'Вашингтон'];
-  var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко',
-    'Топольницкая', 'Нионго', 'Ирвинг'];
+  // var WIZARD_FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия',
+  //   'Люпита', 'Вашингтон'];
+  // var WIZARD_LAST_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко',
+  //   'Топольницкая', 'Нионго', 'Ирвинг'];
 
   var COLOR_STEEL_BLUE = 'rgb(101, 137, 164)';
   var COLOR_DARK_PINK = 'rgb(241, 43, 107)';
@@ -29,9 +29,30 @@
   var renderWizard = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
+  };
+
+  var successHandler = function (wizards) {
+    var fragment = document.createDocumentFragment();
+    for (var j = 0; j < 4; j++) {
+      fragment.appendChild(renderWizard(wizards[j]));
+    }
+    similarWizardList.appendChild(fragment);
+    window.util.showElement(userDialog.querySelector('.setup-similar'));
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+    node.style.lineHeight = 2.0;
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
   };
 
   var fillElement = function (element, color) {
@@ -41,25 +62,27 @@
     element.style.backgroundColor = color;
   };
 
-  var wizards = [];
-  for (var i = 0; i < 4; i++) {
-    wizards.push({
-      name: window.util.getRandomFromArray(WIZARD_FIRST_NAMES) + ' ' + window.util.getRandomFromArray(WIZARD_LAST_NAMES),
-      coatColor: window.util.getRandomFromArray(WIZARD_COAT_COLORS),
-      eyesColor: window.util.getRandomFromArray(WIZARD_EYE_COLORS)
-    });
-  }
+  // var wizards = [];
+  // for (var i = 0; i < 4; i++) {
+  //   wizards.push({
+  //     name: window.util.getRandomFromArray(WIZARD_FIRST_NAMES) + ' ' + window.util.getRandomFromArray(WIZARD_LAST_NAMES),
+  //     coatColor: window.util.getRandomFromArray(WIZARD_COAT_COLORS),
+  //     eyesColor: window.util.getRandomFromArray(WIZARD_EYE_COLORS)
+  //   });
+  // }
+
+  window.backend.load(successHandler, errorHandler);
+  var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    window.backend.save(new FormData(form), function () {
+      window.util.hideElement(userDialog);
+    }, errorHandler);
+  });
 
   window.colorizeElement(coatColorSetup, WIZARD_COAT_COLORS, fillElement);
   window.colorizeElement(eyeColorSetup, WIZARD_EYE_COLORS, fillElement);
   window.colorizeElement(fireballColorSetup, WIZARD_FIREBALL_COLORS, changeElementBackground);
-
-  var fragment = document.createDocumentFragment();
-  for (var j = 0; j < wizards.length; j++) {
-    fragment.appendChild(renderWizard(wizards[j]));
-  }
-  similarWizardList.appendChild(fragment);
-  window.util.showElement(userDialog.querySelector('.setup-similar'));
 
   // Implement drag&drop functionality for wizard artifacts
   shopElement.addEventListener('dragstart', function (event) {
